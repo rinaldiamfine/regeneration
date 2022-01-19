8c3497b5907701633986e0036f0bbe8291f29d77
Rinaldi <rinaldiamfine@gmail.com>
2022-01-20 00:24:29 +0700

    iOS - XCode Project

---
+++ Regeneration/Regeneration/RegenerationApp.swift
@@ -0,0 +1,20 @@
+//
+//  RegenerationApp.swift
+//  Regeneration
+//
+//  Created by Rinaldi LNU on 20/01/22.
+//
+
+import SwiftUI
+
+@main
+struct RegenerationApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
